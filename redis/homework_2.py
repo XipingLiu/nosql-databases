23751008a64a56fa1c987f6922000b7958ad769d
Xiping Liu <xl2639@columbia.edu>
2018-04-07 00:09:48 -0400

    Add homework_2 submission

--- redis/homework_2.py
+++ redis/homework_2.py
@@ -1,3 +1,6 @@
+# Homework 2 - Xiping Liu (xl2639)
+# How to run this script: python3 homework_2.py
+
 import redis
 import datetime
 
@@ -16,7 +19,19 @@ def article_vote(redis, user, article):
 
 def article_switch_vote(redis, user, from_article, to_article):
     # HOMEWORK 2 Part I
-    pass
+    cutoff = datetime.datetime.now() - datetime.timedelta(seconds=ONE_WEEK_IN_SECONDS)
+
+    if not datetime.datetime.fromtimestamp(redis.zscore('time:', from_article)) < cutoff \
+    and not datetime.datetime.fromtimestamp(redis.zscore('time:', to_article)) < cutoff:
+        from_article_id = from_article.split(':')[-1]
+        to_article_id = to_article.split(':')[-1]
+
+        if redis.smove('voted:' + from_article_id, 'voted:' + to_article_id, user):
+            redis.zincrby(name='score:', value=from_article, amount=-VOTE_SCORE)
+            redis.hincrby(name=from_article, key='votes', amount=-1)
+            redis.zincrby(name='score:', value=to_article, amount=VOTE_SCORE)
+            redis.hincrby(name=to_article, key='votes', amount=1)
+
 
 redis = redis.StrictRedis(host='localhost', port=6379, db=0)
 # user:3 up votes article:1
@@ -31,4 +46,5 @@ article_switch_vote(redis, "user:2", "article:8", "article:1")
 # HOMEWORK 2 Part II
 # article = redis.?
 # print redis.?
-
+article = redis.zrangebyscore('score:', 10, 20)[0]
+print(redis.hget(article, 'link').decode('utf-8'))
